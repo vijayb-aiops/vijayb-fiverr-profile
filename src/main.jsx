2afdb0ca08917315280c1a65ed9f8a24463c2ef1
Vijaya Rajeev Bollu <bvijaycloud@gmail.com>
2026-04-01 20:05:24 -0400

    Switch to v2 artifact (Claude version)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FiverrKitV3Artifact from "../fiverr_kit_v3_artifact.jsx";
+import FiverrKitV3Artifact from "../fiverr_kit_v2_artifact.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
